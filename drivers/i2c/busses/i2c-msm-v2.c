MotorolaMobilityLLC/kernel-msm

i2c: msm: Software reset the QUP on remove
When the driver is being removed, software reset the QUP core.
This solves an issue where other drivers (like SPI) could not use
the hardware later.

Moto-CRs-fixed: (CR)
Change-Id: I45271edc38d8899d3555570d08bc03eb6cf94af2
Signed-off-by: Greg Meiste <w30289@motorola.com>
Reviewed-on: http://gerrit.mot.com/887764
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: James Wylder <jwylder@motorola.com>
Reviewed-by: Kenneth Kessler <kennykessler@motorola.com>
Reviewed-by: Jason Knopsnyder <jasonk@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

drivers/i2c/busses/i2c-msm-v2.c
@@ -2992,6 +2992,7 @@ mem_err:
 static int i2c_msm_remove(struct platform_device *pdev)
 {
 	struct i2c_msm_ctrl *ctrl = platform_get_drvdata(pdev);
+	int ret;
 
 	/* Grab mutex to ensure ongoing transaction is over */
 	mutex_lock(&ctrl->xfer.mtx);
@@ -3005,6 +3006,29 @@ static int i2c_msm_remove(struct platform_device *pdev)
 	i2c_msm_dma_teardown(ctrl);
 	i2c_msm_dbgfs_teardown(ctrl);
 	i2c_msm_rsrcs_irq_teardown(ctrl);
+
+	/* vote for clock to allow reset of core */
+	i2c_msm_clk_path_vote(ctrl);
+
+	ret = i2c_msm_pm_clk_prepare_enable(ctrl);
+	if (ret) {
+		dev_err(ctrl->dev, "error in enabling clocks:%d\n", ret);
+		goto clk_err;
+	}
+
+	/*
+	 * Reset the core before teardown. This solves an issue where other
+	 * drivers could not use the hardware.
+	 */
+	ret = i2c_msm_qup_sw_reset(ctrl);
+	if (ret)
+		dev_err(ctrl->dev, "error error on qup software reset\n");
+
+	i2c_msm_pm_clk_disable_unprepare(ctrl);
+
+clk_err:
+	i2c_msm_clk_path_unvote(ctrl);
+
 	i2c_msm_rsrcs_clk_teardown(ctrl);
 	i2c_msm_rsrcs_mem_teardown(ctrl);
 	i2x_msm_blk_free_cache(ctrl);

eva0034/android_kernel_xiaomi_msm8953

drivers/i2c/busses/i2c-msm-v2.c
@@ -2968,6 +2968,7 @@ mem_err:
 static int i2c_msm_remove(struct platform_device *pdev)
 {
 	struct i2c_msm_ctrl *ctrl = platform_get_drvdata(pdev);
+	int ret;
 
 	/* Grab mutex to ensure ongoing transaction is over */
 	mutex_lock(&ctrl->xfer.mtx);
@@ -2981,6 +2982,34 @@ static int i2c_msm_remove(struct platform_device *pdev)
 	i2c_msm_dma_teardown(ctrl);
 	i2c_msm_dbgfs_teardown(ctrl);
 	i2c_msm_rsrcs_irq_teardown(ctrl);
+
+	/* vote for clock to allow reset of core */
+	i2c_msm_clk_path_vote(ctrl);
+
+	ret = i2c_msm_pm_clk_prepare(ctrl);
+	if (ret)
+		goto clk_err;
+
+	ret = i2c_msm_pm_clk_enable(ctrl);
+	if (ret) {
+		dev_err(ctrl->dev, "error in enabling clocks:%d\n", ret);
+		goto clk_err;
+	}
+
+	/*
+	 * Reset the core before teardown. This solves an issue where other
+	 * drivers could not use the hardware.
+	 */
+	ret = i2c_msm_qup_sw_reset(ctrl);
+	if (ret)
+		dev_err(ctrl->dev, "error error on qup software reset\n");
+
+	i2c_msm_pm_clk_disable(ctrl);
+	i2c_msm_pm_clk_unprepare(ctrl);
+
+clk_err:
+	i2c_msm_clk_path_unvote(ctrl);
+
 	i2c_msm_rsrcs_clk_teardown(ctrl);
 	i2c_msm_rsrcs_mem_teardown(ctrl);
 	i2x_msm_blk_free_cache(ctrl);